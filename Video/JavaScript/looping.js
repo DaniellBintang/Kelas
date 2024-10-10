dc21d37a9276cd9e3e1a8ed879fda9c29f630d9f
Daniel Bintang Pratama <123884580+DaniellBintang@users.noreply.github.com>
2024-10-10 11:17:31 +0700

    Add files via upload

---
+++ Video/JavaScript/looping.js
@@ -0,0 +1,18 @@
+let mulai = 0;
+let selesai = 10;
+
+while (mulai <= selesai) {
+  console.log(mulai);
+
+  mulai++;
+}
+
+let start = 100;
+let finish = 10;
+
+do {
+  console.log(start);
+  start++;
+} while (start < finish);
+{
+}
---
+++ Video/JavaScript/tipedata.js
@@ -0,0 +1,31 @@
+let angka = 10;
+
+let kata = "Belajar JavaScript";
+
+let pecahan = 2.5;
+
+console.log(angka);
+
+console.log(typeof angka);
+
+console.log(1 / 0);
+
+console.log("JavaScript" / 2 + 5);
+
+let isi = "Saya";
+
+console.log(isi + kata);
+
+console.log(`'SMK Revit' ${angka} ${kata} ${isi}`);
+
+console.log("SMK Revit" + angka + kata + " " + isi);
+
+let kondisi = true;
+
+console.log(kondisi);
+
+let age = null;
+
+console.log(age);
+
+let kosong;
