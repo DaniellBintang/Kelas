b25bc16fc57c750b41456065f29524ad3a46ef66
Daniel Bintang Pratama <123884580+DaniellBintang@users.noreply.github.com>
2024-11-11 00:16:06 +0700

    Add files via upload

---
+++ Kelas/2024-11-05/pages/tentang.php
@@ -0,0 +1,14 @@
+<?php
+
+$sql = "SELECT * FROM tentang";
+$hasil = mysqli_query($koneksi, $sql);
+
+while ($row = mysqli_fetch_row($hasil)) {
+?>
+    <div class="tentang">
+        <h2><?= $row[1] ?></h2>
+        <p><?= $row[2] ?></p>
+    </div>
+<?php
+}
+?>
